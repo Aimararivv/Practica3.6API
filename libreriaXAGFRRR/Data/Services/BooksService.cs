0dbc0341612475598e5601b975179a2d88376107
Aimararivv <xitziafierro@gmail.com>
2024-11-14 23:35:25 -0700

    Obtener autores con HttpGet

--- libreriaXAGFRRR/Data/Services/BooksService.cs
+++ libreriaXAGFRRR/Data/Services/BooksService.cs
@@ -45,7 +45,23 @@ namespace libreriaXAGFRRR.Data.Services
         //Metodo que nos permite obtener una lista de todos los libros de la BD
         public List<Books> GetAllBks() => _context.Books.ToList();
         //Metodo que nos permite obtener el libro que estamos pidiendo de la BD
-        public Books GetBookById(int bookid) => _context.Books.FirstOrDefault(n => n.id == bookid);
+        public BookWithAuthorsVM GetBookById(int bookid)
+        {
+            var _bookWithAuthors = _context.Books.Where(n => n.id == bookid).Select(book => new BookWithAuthorsVM()
+            {
+                Titulo = book.Titulo,
+                Descripcion = book.Descripcion,
+                ISRead = book.ISRead,
+                DateRead = book.DateRead,
+                Rate = book.Rate,
+                Genero = book.Genero,
+                CoverUrl = book.CoverUrl,
+                PublisherName = book.Publisher.Name,
+                AutorNames = book.book_Authors.Select(n => n.Author.FullName).ToList()
+            }) .FirstOrDefault();
+            return _bookWithAuthors;
+        }
+
         //Metodo que nos permite modificar un libro que se encuentra en la BD
         public Books UpdateBookByID(int bookid, BookVM book)
         {
